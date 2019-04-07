22bb26b899a8a68af2236d05018eeae8cd83bbaa
Umesh Bansari <49323987+umeshbansari@users.noreply.github.com>
2019-04-08 00:42:55 +0500

    lab 1

---
+++ Umesh Rollno 18sw18/Information.java
@@ -0,0 +1,33 @@
+class Information 
+{
+public static void main (String arg [])
+{
+String Name = "umesh_bansari";
+int  Totalmarks= 500;
+int Obtainedmarks=495;
+char Grade = 'A';
+int Percentage;
+Percentage = (int )(Obtainedmarks*100)/   Totalmarks;
+boolean T = true;
+boolean F = false;
+
+
+if ((Percentage>75)==true)
+{
+	System.out.println( "Pass");
+}
+else {
+	System.out.println( "Fail");
+
+}
+
+System.out.println("Name =" + "=" + Name  );
+System.out.println( " Totalmarks" + "=" +  Totalmarks );
+System.out.println( "Obtainedmarks "+ "=" + Obtainedmarks);
+System.out.println( "Grade" + "=" + Grade);
+System.out.println(" Percentage" + "=" + Percentage + "%");
+System.out.println("status" + "="+ Status);
+}	
+
+
+}
---
+++ Umesh Rollno 18sw18/Task2.java
@@ -0,0 +1,20 @@
+
+
+class Task2
+{
+public static void main (String arg [])
+{
+double a = 50.28;
+int b = (int)a;
+double  c =a-b;
+
+System.out.println( "Actual value"+ a);
+System.out.println(" Integer value"+ b);
+
+System.out.println("Floating value"+c);
+
+
+
+}
+
+}
